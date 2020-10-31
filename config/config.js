df96c0833f79055ce94540963f649bd330d1ad9b
motokovil <65578314+motokovil@users.noreply.github.com>
2020-10-31 11:29:10 -0500

    OK

--- config/config.js
+++ config/config.js
@@ -1,4 +1,3 @@
-const { request } = require("express")
 
 require('dotenv').config()
 module.exports = {
@@ -17,10 +16,10 @@ module.exports = {
     "dialect": "postgres"
   },
   production: {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   }
 }
