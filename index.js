33abfbaa525e04746c69a988712ece8c7209a5e7
motokovil <65578314+motokovil@users.noreply.github.com>
2020-11-01 13:17:26 -0500

    ok

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const server = require('./server')
+require('dotenv').config()
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 8000
 server.listen(port, ()=>{
     console.log('Puerto '+port)
 })
